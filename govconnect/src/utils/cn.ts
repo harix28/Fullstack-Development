536f179760cd442ffa34e01521c00ced9d24e51d
Hari Sharma <hsharmax38@gmail.com>
2026-09-18 13:23:52 +0530

    Delete govconnect directory

--- govconnect/src/utils/cn.ts
+++
@@ -1,4 +0,0 @@
-// Utility: merge class names (lightweight cn helper)
-export function cn(...classes: (string | undefined | null | false)[]): string {
-  return classes.filter(Boolean).join(' ');
-}
